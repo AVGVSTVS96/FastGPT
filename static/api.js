38487acac52d214453da86a702b0e72ba4e65c5a
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-06-05 12:52:02 -0400

    formatted code

--- static/api.js
+++ static/api.js
@@ -1,35 +1,35 @@
 async function postRequest() {
     return await fetch("/gpt4", {
-      method: "POST",
-      body: JSON.stringify({
-        messages: messages,
-        model_type: modelName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+        method: "POST",
+        body: JSON.stringify({
+            messages: messages,
+            model_type: modelName,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
     });
-  }
-  
-  async function handleResponse(response, messageText) {
+}
+
+async function handleResponse(response, messageText) {
     const reader = response.body.getReader();
     const decoder = new TextDecoder("utf-8");
     let assistantMessage = "";
-  
+
     while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        messages.push({
-          role: "assistant",
-          content: assistantMessage,
-        });
-        break;
-      }
-  
-      const text = decoder.decode(value);
-      assistantMessage += text;
-      messageText.innerHTML = window.renderMarkdown(assistantMessage).trim();
-      highlightCode(messageText);
-      autoScroll();
+        const { value, done } = await reader.read();
+        if (done) {
+            messages.push({
+                role: "assistant",
+                content: assistantMessage,
+            });
+            break;
+        }
+
+        const text = decoder.decode(value);
+        assistantMessage += text;
+        messageText.innerHTML = window.renderMarkdown(assistantMessage).trim();
+        highlightCode(messageText);
+        autoScroll();
     }
-  }
+}

816eadacc97c9458f873ca8520ba422ace6b66b7
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-06-05 19:32:47 -0400

    refactored, named scroll event  function

--- static/interface.js
+++ static/interface.js
@@ -1,8 +1,10 @@
+// DOM Elements
 const chatMessagesDiv = document.getElementById("chat-messages");
 const userInputElem = document.getElementById("user-input");
 const modelToggle = document.getElementById("model-toggle");
 const modelLabelLeft = document.getElementById("model-label-left");
 const modelLabelRight = document.getElementById("model-label-right");
+
 // State variables
 let modelName = modelToggle.checked ? "gpt-4" : "gpt-3.5-turbo";
 let messages = [];
@@ -10,51 +12,7 @@ let systemMessageRef = null;
 let autoScrollState = true;
 let lastScrollTop = 0;
 
-// Event listener functions
-function handleModelToggle() {
-  if (modelToggle.checked) {
-    modelLabelRight.textContent = "GPT-4";
-    modelName = "gpt-4";
-  } else {
-    modelLabelLeft.textContent = "GPT-3.5";
-    modelName = "gpt-3.5-turbo";
-  }
-}
-
-function handleInputKeydown(event) {
-  if (event.key === "Enter" && !event.shiftKey) {
-    event.preventDefault();
-    document.getElementById("submitBtn").click();
-  }
-}
-
-function autoScroll() {
-  if (autoScrollState) {
-    chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
-  }
-}
-
-// Event listeners for functions above
-modelToggle.addEventListener("change", handleModelToggle);
-document.getElementById("user-input").addEventListener("keydown", handleInputKeydown);
-
-document;
-chatMessagesDiv.addEventListener("scroll", function () {
-  const isAtBottom =
-    chatMessagesDiv.scrollHeight - chatMessagesDiv.clientHeight <=
-    chatMessagesDiv.scrollTop + 150;
-
-  // If user scrolls up, turn off auto-scrolling.
-  if (chatMessagesDiv.scrollTop < lastScrollTop) {
-    autoScrollState = false;
-  } else {
-    autoScrollState = isAtBottom;
-  }
-
-  // Update last scroll top value.
-  lastScrollTop = chatMessagesDiv.scrollTop;
-});
-
+// Utility functions
 window.renderMarkdown = function (content) {
   const md = new markdownit();
   return md.render(content);
@@ -86,3 +44,48 @@ function addMessageToDiv(role, content = "") {
 
   return messageText;
 }
+
+// Event listener functions
+function handleModelToggle() {
+  if (modelToggle.checked) {
+    modelLabelRight.textContent = "GPT-4";
+    modelName = "gpt-4";
+  } else {
+    modelLabelLeft.textContent = "GPT-3.5";
+    modelName = "gpt-3.5-turbo";
+  }
+}
+
+function handleInputKeydown(event) {
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    document.getElementById("submitBtn").click();
+  }
+}
+
+function autoScroll() {
+  if (autoScrollState) {
+    chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
+  }
+}
+
+function handleChatScroll() {
+  const isAtBottom =
+    chatMessagesDiv.scrollHeight - chatMessagesDiv.clientHeight <=
+    chatMessagesDiv.scrollTop + 150;
+
+  // If user scrolls up, turn off auto-scrolling.
+  if (chatMessagesDiv.scrollTop < lastScrollTop) {
+    autoScrollState = false;
+  } else {
+    autoScrollState = isAtBottom;
+  }
+
+  // Update last scroll top value.
+  lastScrollTop = chatMessagesDiv.scrollTop;
+}
+
+// Event listeners
+modelToggle.addEventListener("change", handleModelToggle);
+userInputElem.addEventListener("keydown", handleInputKeydown);
+chatMessagesDiv.addEventListener("scroll", handleChatScroll);
